6e8582877f4444e1b9ec23e16b5a83d44fc286c2
vnogueiraa47 <viniciusvje47@gmail.com>
2025-12-08 13:13:05 +0000

    Final ZOO

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -19,13 +19,55 @@ class Animal(ABC):
 
 class Leao(Animal):
     
-    def __init__(self, nome: str, som: str, mov: str):
-        
+    def __init__(self, nome: str):
         super().__init__(nome)
-        self.som = som
-        self.mov = mov
+    
+    def fazer_som(self):
+        print("SOM LEÃO")
+    
+    def mover(self):
+        print("CORRER")
+
+class Elefante(Animal):
+    def __init__(self, nome: str):
+        super().__init__(nome)
+    
+    def fazer_som(self):
+        print("SOM ELEFANTE")
+    
+    def mover(self):
+        print("ANDA COM TROMBA BALANÇANDO")
+
+class Cobra(Animal):
+    def __init__(self, nome: str):
+        super().__init__(nome)
+    
+    def fazer_som(self):
+        print("SOM DA COBRA")
+
+    def mover(self):
+        print("RASTEJANDO PELO CHÃO")
+
+def apresentar(animal: Animal):
+    print(animal.apresentar_nome())
+    animal.fazer_som()
+    animal.mover()
+    print(f"Tipo do objeto: {type(animal).__name__}")
 
 
 
+animais = [
+    Leao("Leão Africano"),
+    Cobra("Naja"),
+    Elefante("Elefante Asiático")
+]
+
+
+for animal in animais:
+    apresentar(animal)
+
+
+        
+    
 
 
