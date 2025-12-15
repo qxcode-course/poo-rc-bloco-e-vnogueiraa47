34c0c63fdaeff1ae26563b629a4fd713356de939
vnogueiraa47 <viniciusvje47@gmail.com>
2025-12-15 02:11:33 +0000

    Inicio Estacionamento

---
+++ tasks/poo/estacionamento/py/draft.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+
+class Veiculo(ABC):
+    def __init__(self, id: str, tipo: str):
+        self.id = id
+        self.tipo = tipo
+        self.hora = 0
+    
+    def setEntrada(self, horaEntrada: int):
+        self.hora = horaEntrada
+    def getEntrada(self):
+        return self.hora
+
+    def getTipo(self):
+        return self.tipo
+    def getId(self):
+        return self.id
+
+    @abstractmethod
+    def calcularValor(self, horaSaida: int):
+        pass
+
+    def __str__(self):
+        return f"______{self.getTipo()} : ______{self.getId()} \n Hora atual: {self.getEntrada()}"
+
+class Bike(Veiculo):
+    def __init__(self, id: Veiculo):
+        super().__init__(id, "Bike")
+    
+    def calcularValor(self):
+        valor = 3
+        return valor
+
+class Moto(Veiculo):
+    def __init__(self, id: Veiculo):
+        super().__init__(id, "Moto")
+    
+    def calcularValor(self, horaSaida: int):
+        tempo = horaSaida
+        valor = tempo/20
+        return valor
+    
+class Carro(Veiculo):
+    def __init__(self, id: Veiculo):
+        super().__init__(id, "Carro")
+    
+    def calcularValor(self, horaSaida: int):
+        tempo = horaSaida
+        if tempo <5:
+            return
+        else:
+            valor = tempo/10
+            return valor
+        
+        
